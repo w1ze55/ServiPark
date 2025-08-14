82e6a186ef75d92657ab857a7ddb66f267003f7b
w1ze55 <cacauezin@gmail.com>
2025-08-14 15:55:21 -0300

    Backend base added

---
+++ servipark/src/main/java/com/servipark/servipark/ServiparkApplication.java
@@ -0,0 +1,13 @@
+package com.servipark.servipark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiparkApplication.class, args);
+	}
+
+}
---
+++ servipark/src/test/java/com/servipark/servipark/ServiparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servipark.servipark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
